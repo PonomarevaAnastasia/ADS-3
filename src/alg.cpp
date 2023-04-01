32d6109a96bce5885f46f5852fb621fb6f2c085b
PonomarevaAnastasia <116709050+PonomarevaAnastasia@users.noreply.github.com>
2023-04-01 13:43:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,30 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int left = 0;
+  int right = size - 1;
+  int count = 0;
+  while (left <= right) {
+    int mid = (left + right) / 2;
+    if (arr[mid] < value) {
+      left = mid + 1;
+    } else if (arr[mid] > value) {
+      right = mid - 1;
+    } else if (arr[mid] == value) {
+      count++;
+      int newmid = mid - 1;
+      while (arr[newmid] == value && newmid >= start) {
+        count++;
+        newmid--;
+      }
+      newmid = mid + 1;
+      while (arr[newmid] == value && newmid <= end) {
+        count++;
+        newmid++;
+      }
+      return count;
+    }
+  }
+    
   return 0; // если ничего не найдено
 }
