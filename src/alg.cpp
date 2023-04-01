3f867f2eca2aa762ca67bdb248bc8e9b93830497
PonomarevaAnastasia <116709050+PonomarevaAnastasia@users.noreply.github.com>
2023-04-01 13:25:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,4 +3,28 @@
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
   return 0; // если ничего не найдено
+    int left = 0;
+    int right = size - 1;
+    int count = 0;
+    while (right >= left) {
+        int mid = (right + left) / 2;
+        if (*(arr + mid) == value) {
+            count += 1;
+            int newmid = mid;
+            while ((*(arr + mid + 1)) == value) {
+                count += 1;
+                mid += 1;
+            }
+            while ((*(arr + newmid - 1)) == value) {
+                count += 1;
+                newmid -= 1;
+            }
+            return count;
+        } else if (*(arr + mid) > value) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return count;
 }
